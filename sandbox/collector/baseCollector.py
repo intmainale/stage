bbb55f532a60154f09532628322fff17b557cf16
intmainale <alessandro.savio04@gmail.com>
2026-05-07 17:43:07 +0200

    FIX: normalizer into json like structure FIX: influxdb setup ADD: new metrics to be tracked DEL: old sandbox enviroment

--- sandbox/collector/baseCollector.py
+++
@@ -1,9 +0,0 @@
-class BaseCollector:
-    def __init__(self, logger):
-        self.logger = logger
-
-    def collect(self):
-        raise NotImplementedError
-
-    def parse(self, raw):
-        raise NotImplementedError
